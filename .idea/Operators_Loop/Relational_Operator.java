9dfa3f0786b17a32ff37c14c9f0b5c9abdaa85de
Aman Verma <akshuavr@Amans-MacBook-Air.local>
2025-06-12 09:37:27 +0530

    Initial commit

---
+++ .idea/Operators_Loop/Relational_Operator.java
@@ -0,0 +1,10 @@
+
+public class Relational_Operator {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 55;
+        boolean result;
+        result = a != b;
+        System.out.println(result);
+    }
+}
